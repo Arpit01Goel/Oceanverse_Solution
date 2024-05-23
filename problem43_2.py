ff1dbbf7cf9ac3b639c8cfeb72663269e799f306
Arpit <arpit01goel@gmail.com>
2024-05-23 23:04:21 +0530

    Renamed the files and removed wrong codes

--- problem43_2.py
+++
@@ -1,3 +0,0 @@
-#Consider the sensex data from here. Consider only the end of the day closing values. When should you have bought and when should you have sold in order to gain the maximum return in this 30 year period? You can assume that you can buy the stocks partially and you start with, let us say 1,00,000 Rupees.
-
-#import data from csv file 
